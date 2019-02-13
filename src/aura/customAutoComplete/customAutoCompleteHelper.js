649bf2cd517b1cff403a1133d5a771a1937e8e99
Angel Robles <angel.robles@accuv.com>
2019-02-13 13:37:21 -0400

    add alternateFieldLabel improve some performance

--- src/aura/customAutoComplete/customAutoCompleteHelper.js
+++ src/aura/customAutoComplete/customAutoCompleteHelper.js
@@ -12,14 +12,27 @@
             if(helper.isArray(sobjects)){
                 for(var i =0;i<sobjects.length;i++){
                     var aRecord = sobjects[i];
+                   results.push({label:helper.getValueWithTraversing(component.get('v.labelField'), aRecord, helper), value:helper.getValueWithTraversing(component.get('v.valueField'),aRecord, helper), alternate:helper.getValueWithTraversing(component.get('v.alternateFieldLabel'),aRecord, helper)});
                     
-                    if(aRecord.hasOwnProperty(component.get('v.labelField')) && aRecord.hasOwnProperty(component.get('v.valueField'))){
-                        results.push({label:aRecord[component.get('v.labelField')], value:aRecord[component.get('v.valueField')]});
-                    }
                 }
             }
         } 
         return results;
+    },
+    getValueWithTraversing: function(fieldPathToGetValueFor, aRecord, helper){ 
+        if(helper.hasInnerProperty(aRecord,fieldPathToGetValueFor)) return aRecord[fieldPathToGetValueFor];
+        var arr = fieldPathToGetValueFor != null && fieldPathToGetValueFor != undefined ? fieldPathToGetValueFor.split('.') : [];
+         var temp = aRecord;
+        var finalVal = null;
+        for(var i=0;i<arr.length; i++){
+            temp = temp !=  null ?  (helper.hasInnerProperty(temp, arr[i]) ? temp[arr[i]] : null ) : null;
+            if( i == (arr.length -1) ) finalVal = temp;
+        }
+        return finalVal;
+        
+    },
+    hasInnerProperty: function(obj, aProperty){
+        return obj != null && obj != undefined && typeof(obj.hasOwnProperty) == 'function' && obj.hasOwnProperty(aProperty);
     },
     isArray:function(obj){
       return Object.prototype.toString.call(obj) === '[object Array]';  
@@ -27,24 +40,35 @@
     generateQuery:function(q,component, event, helper){
         var customWhere = component.get('v.customCondition');
         var customLimit = component.get('v.itemLimit');
-        var labelField = component.get('v.labelField');
-        var valueField = component.get('v.valueField');
+		var latestFields = helper.generateUniqueFields(component, event, helper);
      
-        var query = 'select '+labelField + (labelField != null && labelField != undefined && labelField.includes(valueField) ? '' : ','+valueField)+' from '+
+        var query = 'select '+latestFields+' from '+
             component.get('v.targetObject')+ ' where '+ component.get('v.labelField')+ " like '%"+q+"%'" + (customWhere != null && customWhere != undefined ? ' and '+ customWhere : '');
         query += ' limit ' +(customLimit != null && customLimit != undefined ? customLimit : '7');
        return query;
     },
     generateRecordByIdQuery:function(idValue, component, event, helper){
-        var labelField = component.get('v.labelField');
-        var valueField = component.get('v.valueField');
-      var query = 'select '+ labelField + ' , '+ valueField + ' from ' + component.get('v.targetObject') +" where Id ='"+ idValue+"'";
+        var latestFields = helper.generateUniqueFields(component, event, helper);
+        var query = 'select '+ latestFields+' from ' + component.get('v.targetObject') +" where Id ='"+ idValue+"'";
        return query; 
+    },
+    generateUniqueFields:function(component, event, helper){
+      var labelField = component.get('v.labelField');
+      var valueField = component.get('v.valueField');
+      var alternateFieldLabel = component.get('v.alternateFieldLabel');
+      var finalFields = [];
+      var uniqueFields={};
+      if(labelField != undefined && labelField != null) uniqueFields[labelField.split(' ').join('')]   = labelField.split(' ').join('');
+      if(valueField != undefined && valueField != null)  uniqueFields[valueField.split(' ').join('')]   = valueField.split(' ').join('');
+      if(alternateFieldLabel != undefined && alternateFieldLabel != null) uniqueFields[alternateFieldLabel.split(' ').join('')]   = alternateFieldLabel.split(' ').join('');
+        for(var key in uniqueFields)finalFields.push(  key.toLowerCase()  );
+        
+      return finalFields.join(',');
     },
     runQuery:function(query,component, event, helper, callback){
         // create a one-time use instance of the serverEcho action
         // in the server-side controller
-       
+        //console.log(query);       
         var action = component.get("c.filter");
         action.setParams({ q:query });
         
